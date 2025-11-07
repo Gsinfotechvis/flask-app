76b16976dd5de545f85b426bca32696d377aae67
BLUEDROID <akshaykapse2002@gmail.com>
2025-11-07 15:52:32 +0530

    new code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "it's lamborgini"
+    return "my fav car is suuuuuuuppppraaaaaaaaaaaa su su su"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
